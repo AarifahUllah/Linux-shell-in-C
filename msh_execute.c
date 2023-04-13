c145df396999f1ea6366e3e4e0a447820086dfa0
Aarifah Ullah <aarifahullah@gwu.edu>
2023-04-13 09:46:35 -0400

    msh_execute: attempt to support multiple pipes

--- msh_execute.c
+++ msh_execute.c
@@ -32,26 +32,23 @@ msh_pipeline_parse(struct msh_pipeline *p)
 	return counter;
 }
 
-//msh_execute executes the parsed pipeline and returns after the pipeline completes
-//M2: background pipes
 void
 msh_execute(struct msh_pipeline *p)
 {
-	//determine how many "|" separators there are
-	//or in other words, how many commands there a
-	int commmand_count = msh_pipeline_parse(p);
+	char * program; //read pipeline's program
+	struct msh_command * c; //retrieve the commands
+	char ** args_list; //arguments list
+
+	//determine how many commands there are
+	int command_count = msh_pipeline_parse(p);
+
 
 	//no forks are required if there is one program
-	if(commmand_count == 1)
+	if(command_count == 1)
 	{
-		//read pipeline's program
-		char * program = msh_command_program(p->pipeline_commands[0]);
-
-		//retrieve the commands
-		struct msh_command * c = msh_pipeline_command(p, 0);
-
-		//arguments list
-		char ** args_list = msh_command_args(c);
+		program = msh_command_program(p->pipeline_commands[0]);
+	    c = msh_pipeline_command(p, 0);
+		args_list = msh_command_args(c);
 
 		//execute the one program
 		if(execvp(program, args_list))
@@ -61,131 +58,280 @@ msh_execute(struct msh_pipeline *p)
 		}
 	}
 
-	//there is more than one program
-	else
+	pid_t pid; //process ID
+	int status; //for parent to wait on the children
+	
+	//make (command_count - 1) amount of pipes 
+	//each of the pipes has 2 fds
+	int fds[2*(command_count - 1)]; 
+	
+	//set up each of the pipes
+	int j;
+	for(j = 0; j < (2*(command_count - 1)); j+=2)
 	{
-		pid_t pid; //process ID
-
-		//set up the pipe
-		int fds[2];
-
-		if(pipe(fds) == -1)
+		if(pipe(fds + j) == -1)
 		{
 			perror("pipe creation, opening pipe");
 			exit(EXIT_FAILURE);
 		}
+	}
 
-		//for loop based on command_count
-		int i; //i is for how many commands there are p[0], p[1], etc.
-		//and also how many forks there are
-		for(i = 0; i < commmand_count; i++)	
+	//fork() when there is more than one program
+	int i;
+	for(i = 0; i < command_count; i++)
+	{
+		pid = fork();
+	}
+
+	if(pid == -1)
+	{
+		perror("forking process");
+		exit(EXIT_FAILURE);
+	}
+
+	//first fork
+	else if (pid == 0)
+	{
+		dup2(fds[1], 1);
+
+		int t;
+		for(t = 0; t < 2*(command_count-1); t++)
 		{
-			pid = fork();
+			close(fds[t]);
+		}
 
-			if(pid == -1)
+		if(execvp(program, args_list))
+		{
+			perror("msh_execute: execvp");
+			exit(EXIT_FAILURE);
+		}
+		exit(EXIT_SUCCESS);
+	}
+
+	//second fork
+	else
+	{
+		if(pid == 0)
+		{
+			dup2(fds[0],0);
+
+			dup2(fds[3],1);
+
+			int r;
+			for(r = 0; r < 2*(command_count-1); r++)
 			{
-				perror("forking process");
-				exit(EXIT_FAILURE);
+				close(fds[r]);
 			}
 
-			//Child process
-			else if(pid == 0)
+			program = msh_command_program(p->pipeline_commands[1]);
+			c = msh_pipeline_command(p, 1);
+			args_list = msh_command_args(c);
+
+	    	if(execvp(program, args_list))
 			{
-				//redirect STDOUT to write side of the pipe we just created.
+				perror("msh_execute: execvp");
+				exit(EXIT_FAILURE);
+			}
+			exit(EXIT_SUCCESS);
+		}
 
-				if(close(fds[0]) == -1) //not reading
+		else
+		{
+			if(pid == 0)
+			{
+				dup2(fds[2], 0);
+				int r;
+				for(r = 0; r < 2*(command_count-1); r++)
 				{
-					perror("close");
-					exit(EXIT_FAILURE);
+					close(fds[r]);
 				}
 
-				if(dup2(fds[1], STDOUT_FILENO == -1))
-				{
-					perror("child dup stdout");
-					exit(EXIT_FAILURE);
-				}
-
-				struct stat buf;
-
-				if(fstat(STDOUT_FILENO, &buf) == -1)
-				{
-					perror("fstat");
-					exit(EXIT_FAILURE);
-				}
-
-				if(!S_ISFIFO(buf.st_mode))
-				{
-					exit(EXIT_FAILURE);
-				}
-
-				//read pipeline's program
-				char * program = msh_command_program(p->pipeline_commands[i]);
-
-				//retrieve the commands
-				struct msh_command * c = msh_pipeline_command(p, i);
-
-				//arguments list
-				char ** args_list = msh_command_args(c);
-
-				//execute the one program
+				program = msh_command_program(p->pipeline_commands[2]);
+				c = msh_pipeline_command(p, 2);
+				args_list = msh_command_args(c);
 				if(execvp(program, args_list))
 				{
 					perror("msh_execute: execvp");
 					exit(EXIT_FAILURE);
 				}
-
-				exit(EXIT_SUCCESS);
-			}
-
-			//Parent process
-			else
-			{
-				//redirect STDIN to reader side of the pipe we just created.
-
-				//not writing, close standard in
-				if(close(STDIN_FILENO) == -1)
-				{
-					exit(EXIT_FAILURE);
-				}
-
-				//replace with input side of the pipe
-				if(dup2(fds[0], STDIN_FILENO) == -1)
-				{
-					perror("parent dup stdin");
-					exit(EXIT_FAILURE);
-				}
-
-				//be sure to close pipes or else
-				//child will always wait for additional input
-
-				//wait for the child process to finish (this changes later for background pipes)
-				waitpid(pid, NULL, 0); //wait() waits for any child process
-				//waitpid() refers to parent
-
-				struct stat sbuf;
-
-				if(fstat(STDIN_FILENO, &sbuf) == -1)
-				{
-					perror("fstat");
-					exit(EXIT_FAILURE);
-				}
-
-				if(!S_ISFIFO(sbuf.st_mode))
-				{
-					exit(EXIT_FAILURE);
-				}
-
-				//scanf and printf here to read and write
-				/*char buf[256];
-				scanf("%s", buf);
-
-				printf("%s\n", buf);*/
 			}
 		}
 	}
 
+	//the parent process, waits for all child processes to finish
+	int k;
+	for(k = 0; k < 2*(command_count-1); k++)
+	{
+		close(fds[k]);
+	}
+
+	int m;
+	for(m = 0; m < command_count; m++)
+	{
+		wait(&status);
+	}
+
 	return;
 }
+
+//msh_execute executes the parsed pipeline and returns after the pipeline completes
+//M2: background pipes
+/*void
+msh_execute(struct msh_pipeline *p)
+{
+	char * program; //read pipeline's program
+	struct msh_command * c; //retrieve the commands
+	char ** args_list; //arguments list
+
+	//determine how many commands there are
+	int commmand_count = msh_pipeline_parse(p);
+
+
+	//no forks are required if there is one program
+	if(commmand_count == 1)
+	{
+		program = msh_command_program(p->pipeline_commands[0]);
+	    c = msh_pipeline_command(p, 0);
+		args_list = msh_command_args(c);
+
+		//execute the one program
+		if(execvp(program, args_list))
+		{
+			perror("msh_execute: execvp");
+			exit(EXIT_FAILURE);
+		}
+	}
+
+	pid_t pid; //process ID
+	struct stat buf;
+	int status; //for parent to wait on the children
+	int i;
+	
+	//make (command_count - 1) amount of pipes 
+	//each of the pipes has 2 fds
+	int fds[2]; //set up the pipe
+
+	if(pipe(fds) == -1)
+	{
+		perror("pipe creation, opening pipe");
+		exit(EXIT_FAILURE);
+	}
+
+	//fork() when there is more than one program
+	int i;
+	for(i = 0; i < commmand_count; i++)
+	{
+		pid = fork();
+	}
+
+	if(pid == -1)
+	{
+		perror("forking process");
+		exit(EXIT_FAILURE);
+	}
+
+	//Child process
+	else if(pid == 0)
+	{
+		//redirect STDOUT to write side of the pipe we just created.
+
+		if(close(STDOUT_FILENO) == -1) //not reading
+		{
+			perror("close");
+			exit(EXIT_FAILURE);
+		}
+
+		dup2(fds[1], STDOUT_FILENO);
+
+		if (close(fds[0]) == -1) {//not reading
+			perror("close");
+			exit(EXIT_FAILURE);
+        }
+
+		if(close(fds[1]) == -1)
+		{
+			perror("close");
+			exit(EXIT_FAILURE);
+		}
+
+		if(fstat(STDOUT_FILENO, &buf) == -1)
+		{
+			perror("fstat");
+			exit(EXIT_FAILURE);
+		}
+
+			if(!S_ISFIFO(buf.st_mode))
+			{
+				exit(EXIT_FAILURE);
+			}
+
+			int k;
+			for(k = 0; k < commmand_count; k++)
+			{
+				program = msh_command_program(p->pipeline_commands[i]); //read pipeline's program
+			
+				c = msh_pipeline_command(p, i); //retrieve the commands
+
+				args_list = msh_command_args(c); //arguments list
+
+				//execute program
+				execvp(program, args_list);
+				exit(EXIT_SUCCESS);
+			}
+	}
+
+	//Parent process
+	else
+	{
+		//redirect STDIN to reader side of the pipe we just created.
+
+		//not writing, close standard in
+		if(close(STDIN_FILENO) == -1)
+		{
+			exit(EXIT_FAILURE);
+		}
+
+		//replace with input side of the pipe
+		if(dup2(fds[0], STDIN_FILENO) == -1)
+		{
+			perror("parent dup stdin");
+			exit(EXIT_FAILURE);
+		}
+
+		//be sure to close pipes or else
+		//child will always wait for additional input
+		if (close(fds[0]) == -1)//not reading
+		{
+			perror("close");
+			exit(EXIT_FAILURE);
+       	}
+
+		if(close(fds[1]) == -1)
+		{
+			perror("close");
+			exit(EXIT_FAILURE);
+		}
+
+		//wait for the child process to finish (this changes later for background pipes)
+		waitpid(pid, NULL, 0); //wait() waits for any child process
+		//waitpid() refers to parent
+
+		if(fstat(STDIN_FILENO, &buf) == -1)
+		{
+			perror("fstat");
+			exit(EXIT_FAILURE);
+		}
+
+		if(!S_ISFIFO(buf.st_mode))
+		{
+			exit(EXIT_FAILURE);
+		}
+		char buf[256];
+		scanf("%s", buf);
+		printf("%s\n", buf);
+	}
+	return;
+}*/
 
 //M2: set up signal handlers here
 void
@@ -193,3 +339,113 @@ msh_init(void)
 {
 	return;
 }
+
+
+
+/*#include <stdio.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+
+
+int main(int argc, char **argv)
+{
+  int status;
+  int i;
+
+  // arguments for commands; your parser would be responsible for
+  // setting up arrays like these
+
+  char *cat_args[] = {"cat", "scores", NULL};
+  char *grep_args[] = {"grep", "Villanova", NULL};
+  char *cut_args[] = {"cut", "-b", "1-10", NULL};
+
+  // make 2 pipes (cat to grep and grep to cut); each has 2 fds
+
+  int pipes[4];
+  pipe(pipes); // sets up 1st pipe
+  pipe(pipes + 2); // sets up 2nd pipe
+
+  // we now have 4 fds:
+  // pipes[0] = read end of cat->grep pipe (read by grep)
+  // pipes[1] = write end of cat->grep pipe (written by cat)
+  // pipes[2] = read end of grep->cut pipe (read by cut)
+  // pipes[3] = write end of grep->cut pipe (written by grep)
+
+  // Note that the code in each if is basically identical, so you
+  // could set up a loop to handle it.  The differences are in the
+  // indicies into pipes used for the dup2 system call
+  // and that the 1st and last only deal with the end of one pipe.
+
+  // fork the first child (to execute cat)
+  
+  if (fork() == 0)
+  {
+	// replace cat's stdout with write part of 1st pipe
+    dup2(pipes[1], 1);
+
+    // close all pipes (very important!); end we're using was safely copied
+
+    close(pipes[0]);
+    close(pipes[1]);
+    close(pipes[2]);
+    close(pipes[3]);
+
+    execvp(*cat_args, cat_args);
+   }
+  else
+  {
+	// fork second child (to execute grep)
+	
+	if (fork() == 0)
+	{
+	  // replace grep's stdin with read end of 1st pipe
+	  
+	  dup2(pipes[0], 0);
+
+	  // replace grep's stdout with write end of 2nd pipe
+
+	  dup2(pipes[3], 1);
+
+	  // close all ends of pipes
+
+	  close(pipes[0]);
+	  close(pipes[1]);
+	  close(pipes[2]);
+	  close(pipes[3]);
+
+	  execvp(*grep_args, grep_args);
+	}
+	
+	else
+	{
+	  // fork third child (to execute cut)
+
+	  if (fork() == 0)
+	  {
+		// replace cut's stdin with input read of 2nd pipe
+
+	    dup2(pipes[2], 0);
+
+	    // close all ends of pipes
+		close(pipes[0]);
+		close(pipes[1]);
+		close(pipes[2]);
+		close(pipes[3]);
+
+	    execvp(*cut_args, cut_args);
+		}
+	}
+    }
+      
+  // only the parent gets here and waits for 3 children to finish
+  
+  close(pipes[0]);
+  close(pipes[1]);
+  close(pipes[2]);
+  close(pipes[3]);
+
+  for (i = 0; i < 3; i++)
+    wait(&status);
+}*/
